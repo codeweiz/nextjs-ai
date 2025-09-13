05a0ad10dd11bcb4f0ee2763a4a61bf4a33e657a
codeweiz <13955645241@163.com>
2025-09-13 11:39:58 +0800

    feat(init): nextjs、turbo 基础搭建

---
+++ apps/meet/src/i18n/request.ts
@@ -0,0 +1,21 @@
+import { getRequestConfig } from "next-intl/server";
+import { getUserLocale } from "@microboat/meet/i18n/locale-cookie";
+import { routing } from "@microboat/meet/i18n/routing";
+import { getMessagesForLocale } from "@microboat/meet/i18n/messages";
+
+export default getRequestConfig(async ({ requestLocale }) => {
+  let requested = await requestLocale;
+  if (!requested) {
+    requested = await getUserLocale();
+  }
+
+  // @ts-ignore
+  if (!routing.locales.includes(requested)) {
+    requested = routing.defaultLocale;
+  }
+
+  return {
+    locale: requested,
+    messages: await getMessagesForLocale(requested),
+  };
+});
---
+++ apps/meet/src/i18n/routing.ts
@@ -0,0 +1,11 @@
+import { defineRouting } from "next-intl/routing";
+
+export const routing = defineRouting({
+  locales: ["zh", "en"],
+  defaultLocale: "en",
+  localeCookie: {
+    name: "NEXT_LOCALE",
+  },
+  localeDetection: true,
+  localePrefix: "as-needed",
+});
